57d19dd374fb66365b340fb8aac4a3cbd2d6c01b
Mary Kilduff <marykilduff@Marys-MacBook-Pro-4.local>
2025-01-09 14:51:53 -0600

    updating css for nav bar and background

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,6 +5,13 @@ import Image from "next/image";
 const NavBar = () => {
   return (
     <>
+      <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]">
+        <Image
+          src={assets.header_bg_color}
+          alt="background"
+          className="w-full"
+        />
+      </div>
       <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50">
         <a href="#top">
           <Image
@@ -13,28 +20,38 @@ const NavBar = () => {
             className="w-28 cursor-pointer mr-14"
           ></Image>
         </a>
-        <ul className="hidden md:flex items-center justify-between gap-6 lg:gap-8 rounded-full px-12 py-3">
+        <ul className="hidden md:flex items-center justify-between gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50">
           <li>
-            <a href="#top">Home</a>{" "}
+            <a className="font-Ovo" href="#top">
+              Home
+            </a>{" "}
           </li>
           <li>
             {" "}
-            <a href="#about">About Me</a>
+            <a className="font-Ovo" href="#about">
+              About Me
+            </a>
           </li>
           <li>
-            <a href="#services">Services</a>
+            <a className="font-Ovo" href="#services">
+              Services
+            </a>
           </li>
           <li>
-            <a href="#projects">Projects</a>
+            <a className="font-Ovo" href="#projects">
+              Projects
+            </a>
           </li>
           <li>
-            <a href="#contact">Contact Me</a>
+            <a className="font-Ovo" href="#contact">
+              Contact Me
+            </a>
           </li>
         </ul>
         <div>
           <a
             href="#contact"
-            className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 "
+            className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo "
           >
             Contact
             <Image
