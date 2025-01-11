eaee4be22b084bba93ebbe91ef567e96357a3f34
Mary Kilduff <marykilduff@Marys-MacBook-Pro-4.local>
2025-01-10 18:46:54 -0600

    adding in jest and first test

---
+++ src/components/navBar.test.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import NavBar from "./NavBar";
+import { describe, expect, it } from "@jest/globals";
+import "@testing-library/jest-dom";
+
+describe("NavBar Compnent", () => {
+  it("should render the NavBar correctly", () => {
+    render(<NavBar />);
+
+    // Check if essential elements are rendered
+    expect(screen.getByText("Contact")).toBeInTheDocument();
+  });
+});
