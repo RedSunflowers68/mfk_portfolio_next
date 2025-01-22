4b853cd885c2702d359c795aea6c3e4c033edad5
Mary Kilduff <marykilduff@Marys-MacBook-Pro-4.local>
2025-01-22 17:41:40 -0600

    adding few more expects

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -5,10 +5,14 @@ import { describe, expect, it } from "@jest/globals";
 import "@testing-library/jest-dom";
 
 describe("Header Component", () => {
-  it("should render the NavBar correctly", () => {
+  it("should render the Header correctly", () => {
     render(<Header />);
 
     // Check if essential elements are rendered
     expect(screen.getByText("Hi! I am Mary Kilduff")).toBeInTheDocument();
+    expect(screen.getByText("Contact Me")).toBeInTheDocument();
+    expect(
+      screen.getByText("Software Developer based in USA")
+    ).toBeInTheDocument();
   });
 });
