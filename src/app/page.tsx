379d406254cb6a6b65c4cdf4203c9c269a64b814
Mary Kilduff <marykilduff@Marys-MacBook-Pro-4.local>
2025-01-23 18:26:24 -0600

    start of work section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import About from "@/components/About/About";
 import Header from "@/components/Header/Header";
 import NavBar from "@/components/NavBar/NavBar";
+import Work from "@/components/Work/Work";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <NavBar />
       <Header />
       <About />
+      <Work /> 
     </>
   );
 }
