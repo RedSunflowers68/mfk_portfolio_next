1640af20ca80663c0d80d2c370e4b6a1b42b8817
Mary Kilduff <marykilduff@Marys-MacBook-Pro-4.local>
2025-01-27 17:30:59 -0600

    adding in transitions for page load

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -17,21 +17,39 @@ const Header = () => {
           className="rounded-full w-32"
         ></Image>
       </motion.div>
-      <h3 className="flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo">
+      <motion.h3
+        className="flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo"
+        initial={{ y: -20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.6, delay: 0.3 }}
+      >
         Hi! I am Mary Kilduff
         <Image src={assets.hand_icon} alt="hand icon" className="w-6" />
-      </h3>
-      <h1 className="text-3xl sm:text-6xl lg:text-[66px] font-Ovo">
+      </motion.h3>
+      <motion.h1
+        className="text-3xl sm:text-6xl lg:text-[66px] font-Ovo"
+        initial={{ y: -20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.6, delay: 0.5 }}
+      >
         Software Developer based in USA
-      </h1>
-      <p className="max-w-2xl mx-auto font-Ovo">
+      </motion.h1>
+      <motion.p
+        className="max-w-2xl mx-auto font-Ovo"
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.6, delay: 0.7 }}
+      >
         I am a Software Developer currently living in Texas, USA with 2.5 years
         of experiance, primarily for consultancies.
-      </p>
+      </motion.p>
       <div className="flex flex-col sm:flex-row items-center gap-4 mt-4">
-        <a
+        <motion.a
           href="#contact"
           className="px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2 dark:bg-transparent"
+          initial={{ y: 30, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.6, delay: 1 }}
         >
           Contact Me
           <Image
@@ -39,15 +57,18 @@ const Header = () => {
             alt="white arrow"
             className="w-4"
           />
-        </a>
-        <a
+        </motion.a>
+        <motion.a
           href="/sample-resume.pdf"
           download
           className="px-10 py-3 border rounded-full border-grey-500 flex items-center gap-2 dark:text-black bg-white"
+          initial={{ y: 30, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.6, delay: 1.2 }}
         >
           My Resume
           <Image src={assets.download_icon} alt="white arrow" className="w-4" />
-        </a>
+        </motion.a>
       </div>
     </div>
   );
